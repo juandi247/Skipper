77ddac0bece06c54e0cb1933b5ae463d793ae775
juandi247 <jdiaz873@unab.edu.co>
2025-05-07 17:06:15 -0500

    deploy: vm for the proxy

---
+++ terraform/googlecloud.tf
@@ -0,0 +1,59 @@
+# Este código es compatible con Terraform 4.25.0 y versiones compatibles con 4.25.0.
+# Para obtener información sobre la validación de este código de Terraform, consulta https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "skippervm" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-20250507-213658"
+
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20250425"
+      size  = 30
+      type  = "pd-standard"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-micro"
+  name         = "skippervm"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/skipper-proxy/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "581304351219-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = false
+    enable_secure_boot          = false
+    enable_vtpm                 = false
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "us-central1-c"
+}
