0d8c0653b661991e1a6e653f2e93daa8e92b086a
juandi247 <jdiaz873@unab.edu.co>
2025-06-21 00:12:19 -0500

    delete: all previous skipper files

---
+++ SkipperProxy/mainl.go
@@ -0,0 +1,10 @@
+package main
+/* this is the new skipper rewrite implementation, using new features and implementing
+TigerStyle inspiration for the project. Im not expert or anything in go and programming but this
+project is focused for learning Golang mainly and experimenting with new low level concepts :)
+Juan Diego Diaz
+*/
+func main() {
+	
+
+}
