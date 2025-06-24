443c69d57546db03c5d0aadb9477fae02f50fe25
juandi247 <jdiaz873@unab.edu.co>
2025-06-23 22:35:53 -0500

    feat: framing constants

--- SkipperProxy/constants/frameConstants.go
+++ SkipperProxy/constants/frameConstants.go
@@ -1,5 +1,7 @@
 package constants
 
+import "fmt"
+
 const SkipperMagic = "SKPR"
 
 // Framing Types
@@ -7,6 +9,15 @@ const (
 	Control_TunnelRequest = iota + 1
 	Control_TunnelAck
 	Control_TunnelError
-	RequestType
-	ResponseType
+	ProxyRequestType
+	TunnelResponseType
+	ProxyPing
+	TunnelPong
 )
+
+func ValidateFramingType(number uint8) error {
+	if number <= 0 || number >= 7 {
+		return fmt.Errorf("frame type unexistent")
+	}
+	return nil
+}
