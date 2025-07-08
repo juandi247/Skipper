b98427b199c2983d3306d1b8596a683e3343ad3c
juandi247 <jdiaz873@unab.edu.co>
2025-07-07 20:49:54 -0500

    feat: show html templates in error cases

--- SkipperProxy/http/handler.go
+++ SkipperProxy/http/handler.go
@@ -15,35 +15,27 @@ func ClosureFunc(tm tunnel.TunnelManager) http.HandlerFunc {
 		subdomain, exists := ParseSubdomain(r.Host)
 
 		if !exists {
-			w.Write([]byte("we are gonna show skipper.lat page"))
+			http.ServeFile(w, r, "templates/index.html")
+			return
 		}
 
-		fmt.Println("we start byt getting the tunnel")
 		connectedTunnel, err := tm.GetTunnel(subdomain)
 
 		if err != nil {
+			http.ServeFile(w, r, "templates/error.html")
 			fmt.Println("the subdomain doenst exist")
-			w.Write([]byte("doesnt exists subdomain!"))
 			return
 		}
 
-		fmt.Println("now we serilaize")
 		responseChannel := make(chan *frame.InternalFrame, 1)
 		nextStreamId := DefineStreamId(connectedTunnel, responseChannel)
+		defer deleteChannelFromMap(connectedTunnel, nextStreamId)
 		finalPayload, payloadLength, err := SerializeHttpRequest(subdomain, r)
-		fmt.Println("VAMOS A VER EL LENGTH DEL PAUYLOAD")
-	
-		fmt.Println(payloadLength)
 
 		if err != nil {
-			fmt.Println("error when serialezing the request", err)
-			w.Write([]byte("error seralizatin the request"))
+			http.ServeFile(w, r, "templates/timeout.html")
 			return
 		}
-		fmt.Println("we serialized the request")
-
-		fmt.Println("stream id defined")
-		// defer deleteChannelFromMap(connectedTunnel, nextStreamId)
 
 		requestFrame := frame.CreateFrame(
 			1,                          //version
@@ -52,39 +44,31 @@ func ClosureFunc(tm tunnel.TunnelManager) http.HandlerFunc {
 			payloadLength)
 
 		buffer := requestFrame.Encode(finalPayload)
-
-	
 		_, err = connectedTunnel.Connection.Write(buffer)
 
 		if err != nil {
-			fmt.Println("ERRRORRRR sedning the request ", err)
+			http.ServeFile(w, r, "templates/timeout.html")
 			return
 		}
-		select {
-		case ResponseFrame := <-responseChannel:
-			Response, err := DeserializeResponse(ResponseFrame.Payload)
-			if err != nil {
-				fmt.Println("ERRORRRR despues de desserliar", err)
-				return
-			}
-			tunnel.InternalPayloadPool.Put(ResponseFrame)
 
-		
-			fmt.Println("cosoo")
-
-			for key, mivalue:=range Response.GetHeaders(){
-				fmt.Println("Para esta key", key)
-				for _, value:= range mivalue.GetHeaderValues(){
-				w.Header().Set(key,value)
-				fmt.Print("valor",value, " ")
-				}
-				fmt.Println("-----------")
-			}
-		
-			w.WriteHeader(int(Response.GetStatusCode()))
-			bodyReader := bytes.NewReader(Response.GetBody())
-			io.Copy(w, bodyReader)
-			
+		ResponseFrame := <-responseChannel
+		Response, err := DeserializeResponse(ResponseFrame.Payload)
+		if err != nil {
+			http.ServeFile(w, r, "templates/timeout.html")
+			fmt.Println("ERRORRRR despues de desserliar", err)
+			return
 		}
+		tunnel.InternalPayloadPool.Put(ResponseFrame)
+
+		for key, mivalue := range Response.GetHeaders() {
+			for _, value := range mivalue.GetHeaderValues() {
+				w.Header().Set(key, value)
+			}
+		}
+
+		w.WriteHeader(int(Response.GetStatusCode()))
+		bodyReader := bytes.NewReader(Response.GetBody())
+		io.Copy(w, bodyReader)
+
 	}
 }
