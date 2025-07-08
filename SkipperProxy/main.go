a6245e34f67f18a60a027a8cee66e8993ca967fc
juandi247 <jdiaz873@unab.edu.co>
2025-07-07 22:30:06 -0500

    chore: https port for proxy

--- SkipperProxy/main.go
+++ SkipperProxy/main.go
@@ -21,7 +21,7 @@ func main() {
 	certFile := "/etc/letsencrypt/live/skipper.lat-0001/fullchain.pem"
 	keyFile := "/etc/letsencrypt/live/skipper.lat-0001/privkey.pem"
 
-	httpServer := http.CreateHttpServer(":8080", tm, true ,certFile, keyFile )
+	httpServer := http.CreateHttpServer(":443", tm, true ,certFile, keyFile )
 	wg.Add(1)
 	go httpServer.StartServer()
 	tcpServer := tcp.CreateTcpServer(":9000", tm)
